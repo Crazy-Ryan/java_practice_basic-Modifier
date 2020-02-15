e4fa1e045ab2ca68deb27c7f519d903fe2312122
Ryan.Wang <ranwang1995@outlook.com>
2020-02-15 23:31:45 +0800

    [WangRan] feat: finish main method in class Application

--- modifier/src/main/java/Application.java
+++ modifier/src/main/java/Application.java
@@ -1,7 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Application {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-  }
+        Student cindy = new Student("Cindy", 80);
+        Student mary = new Student("Mary", 95);
+        Teacher solider = new Teacher("Solider", new ArrayList<>(Collections.singletonList(mary)));
+        Teacher bob = new Teacher("Bob", new ArrayList<>(Collections.singletonList(cindy)));
+        printStudentInfoUnderTeacher(solider);
+        printStudentInfoUnderTeacher(bob);
 
+    }
+
+    public static void printStudentInfoUnderTeacher(Teacher teacher){
+        ArrayList<Student> studentList = teacher.getStudentList();
+        System.out.println("========================");
+        System.out.println("Teacher " + teacher.getName() + " teach(es) this/these student(s)");
+        for (Student student : studentList) {
+            student.setGrade(student.getGrade() + 5);
+            System.out.print("Name:" + student.getName());
+            System.out.println("(Grade:" + student.getGrade() + ")");
+        }
+        System.out.println("========================");
+    }
 }
